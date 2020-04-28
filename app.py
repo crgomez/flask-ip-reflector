64d353129ed7353dcc244d537cf455f87592bc36
Carlos R Gomez <crgomez@users.noreply.github.com>
2020-04-28 14:58:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ app = Flask(__name__)
 def report_ip():
       return request.remote_addr, 200
 
-app.run(host="0.0.0.0", port=80, debug=True)
+# Serving Flask App with Waitress via app-waitress.py
+# app.run(host="0.0.0.0", port=80, debug=True)
